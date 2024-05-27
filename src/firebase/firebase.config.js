3a52829702db9521426559c31904181bb4b08aff
sabbirziauddin <sabbircseiiuc@gmail.com>
2024-05-27 23:42:38 +0200

    remove .env.local from firebase configuration

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,5 +1,5 @@
 
-/* import { initializeApp } from "firebase/app";
+import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_apiKey,
@@ -10,24 +10,5 @@ const firebaseConfig = {
   appId: import.meta.env.VITE_appId,
 };
 
-export const app = initializeApp(firebaseConfig); */
- 
-
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyBjk3DTEfDg0OLh0nY2CulH5vOzrNk0bRM",
-  authDomain: "appointment-service-3dcf4.firebaseapp.com",
-  projectId: "appointment-service-3dcf4",
-  storageBucket: "appointment-service-3dcf4.appspot.com",
-  messagingSenderId: "808536945979",
-  appId: "1:808536945979:web:1c02568ac49c41ab9df31e"
-};
-
-// Initialize Firebase
-
 export const app = initializeApp(firebaseConfig);
+ 
